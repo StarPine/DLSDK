0e8fb02cfe4c5ab9b7341b678971d2d5d4d5c77b
starpine <subong2013@163.com>
2022-03-30 15:42:20 +0800

    域名修改，网络日志打开

--- app/src/main/java/com/dl/playfun/data/source/http/RetrofitClient.java
+++ app/src/main/java/com/dl/playfun/data/source/http/RetrofitClient.java
@@ -3,6 +3,7 @@ package com.dl.playfun.data.source.http;
 import android.content.Context;
 import android.text.TextUtils;
 
+import com.dl.playfun.BuildConfig;
 import com.dl.playfun.app.AppConfig;
 import com.dl.playfun.data.source.http.interceptor.TokenInterceptor;
 import com.ihsanbal.logging.Level;
@@ -85,7 +86,7 @@ public class RetrofitClient {
                 .addInterceptor(new TokenInterceptor())
                 .addInterceptor(new LoggingInterceptor
                         .Builder()//构建者模式
-                        .loggable(false) //是否开启日志打印
+                        .loggable(BuildConfig.DEBUG) //是否开启日志打印
                         .setLevel(Level.BASIC) //打印的等级
                         .log(Platform.INFO) // 打印类型
                         .request("Request") // request的Tag
