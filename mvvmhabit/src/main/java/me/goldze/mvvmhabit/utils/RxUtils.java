49fd0c7d0d457787ee98195e4d1c0f6f967b08c9
15616314565 <15616314565@163.com>
2022-04-08 11:17:28 +0800

    删除RxUtil无用方法。 新增国际化图片包

--- mvvmhabit/src/main/java/me/goldze/mvvmhabit/utils/RxUtils.java
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/utils/RxUtils.java
@@ -1,18 +1,9 @@
 package me.goldze.mvvmhabit.utils;
 
-import android.content.Context;
-import android.util.Log;
-
-import androidx.fragment.app.Fragment;
-
-import com.trello.rxlifecycle2.LifecycleProvider;
-import com.trello.rxlifecycle2.LifecycleTransformer;
-
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import me.goldze.mvvmhabit.http.BaseResponse;
@@ -23,40 +14,7 @@ import me.goldze.mvvmhabit.http.ExceptionHandle;
  * 有关Rx的工具类
  */
 public class RxUtils {
-    /**
-     * 生命周期绑定
-     *
-     * @param lifecycle Activity
-     */
-    public static <T> LifecycleTransformer<T> bindToLifecycle(@NonNull Context lifecycle) {
-        if (lifecycle instanceof LifecycleProvider) {
-            return ((LifecycleProvider) lifecycle).bindToLifecycle();
-        } else {
-            throw new IllegalArgumentException("context not the LifecycleProvider type");
-        }
-    }
 
-    /**
-     * 生命周期绑定
-     *
-     * @param lifecycle Fragment
-     */
-    public static LifecycleTransformer bindToLifecycle(@NonNull Fragment lifecycle) {
-        if (lifecycle instanceof LifecycleProvider) {
-            return ((LifecycleProvider) lifecycle).bindToLifecycle();
-        } else {
-            throw new IllegalArgumentException("fragment not the LifecycleProvider type");
-        }
-    }
-
-    /**
-     * 生命周期绑定
-     *
-     * @param lifecycle Fragment
-     */
-    public static LifecycleTransformer bindToLifecycle(@NonNull LifecycleProvider lifecycle) {
-        return lifecycle.bindToLifecycle();
-    }
 
     /**
      * 线程调度器
