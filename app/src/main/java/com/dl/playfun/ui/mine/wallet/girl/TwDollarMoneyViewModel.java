64dca54740603ff890a243d40a7653ae37aead8a
15616314565 <15616314565@163.com>
2022-05-05 19:44:42 +0800

    修改明细页面没有格式化保留两位小数

--- app/src/main/java/com/dl/playfun/ui/mine/wallet/girl/TwDollarMoneyViewModel.java
+++ app/src/main/java/com/dl/playfun/ui/mine/wallet/girl/TwDollarMoneyViewModel.java
@@ -128,7 +128,7 @@ public class TwDollarMoneyViewModel extends BaseViewModel<AppRepository> {
                 .subscribe(new BaseObserver<BaseDataResponse<UserProfitPageEntity>>() {
                     @Override
                     public void onSuccess(BaseDataResponse<UserProfitPageEntity> response) {
-                        totalProfits.set(String.valueOf(response.getData().getTotalProfits()));
+                        totalProfits.set(String.format("%.2f", response.getData().getTotalProfits()));
                         UserProfitPageEntity.CustomProfitList pageData = response.getData().getUserProfitList();
                     List<UserProfitPageInfoEntity> listData = pageData.getData();
                     if(!ObjectUtils.isEmpty(listData) && listData.size()>0){
