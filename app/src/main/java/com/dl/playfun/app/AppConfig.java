e89668a7e7c3b196684ad8a0db4fe95f9642a513
15616314565 <15616314565@163.com>
2022-05-17 17:19:23 +0800

    调整Im新的客服id

--- app/src/main/java/com/dl/playfun/app/AppConfig.java
+++ app/src/main/java/com/dl/playfun/app/AppConfig.java
@@ -7,7 +7,8 @@ import com.dl.playfun.entity.BannerItemEntity;
  * @author wulei
  */
 public class AppConfig {
-    public static final boolean isDebug = BuildConfig.DEBUG;
+    //是否开启日志打印
+    public static final boolean isDebug = false;
     /**
      * 版本编号
      */
@@ -39,6 +40,10 @@ public class AppConfig {
      * 客服id
      */
     public static final String CHAT_SERVICE_USER_ID = "administrator";
+    /**
+     * 联系客服id
+     */
+    public static final String CHAT_SERVICE_USER_ID_SEND = "administrator_okey101";
     /**
      * 服务端接口HOST
      */
