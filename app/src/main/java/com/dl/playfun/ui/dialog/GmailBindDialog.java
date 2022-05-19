8c4a1248bd35b87303716397b7dbe448879e9faa
15616314565 <15616314565@163.com>
2022-05-19 19:57:32 +0800

    新增绑定邮箱提示弹窗。待完善

---
+++ app/src/main/java/com/dl/playfun/ui/dialog/GmailBindDialog.java
@@ -0,0 +1,17 @@
+package com.dl.playfun.ui.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Author: 彭石林
+ * Time: 2022/5/19 12:20
+ * Description: This is GmailBindDialog
+ */
+public class GmailBindDialog {
+
+    public static Dialog getHintEmailDialog(Context context, boolean touchOutside) {
+
+    }
+
+}
