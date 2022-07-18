3700d058a37251c6310cb47590e326bd6cfae260
15616314565 <15616314565@163.com>
2022-07-18 11:45:04 +0800

    1:修复1v1聊天布局。没有引入包声明 2：调整启动页 红色文字+下划线

--- app/src/PlayChatAAB/java/com/dl/playfun/ui/splash/SplashFragment.java
+++ app/src/PlayChatAAB/java/com/dl/playfun/ui/splash/SplashFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -44,12 +45,13 @@ public class SplashFragment extends BaseFragment<FragmentSplashBinding, SplashVi
         super.initViewObservable();
         String txt = getString(R.string.splash_init_tv);
         String txt2 = getString(R.string.splash_init_tv2);
-        int whiteLength = txt.length()-txt2.length();
+        int whiteLength = txt.length() - txt2.length();
         SpannableString stringBuilder = new SpannableString(txt);
         ForegroundColorSpan whiteSpan = new ForegroundColorSpan(ColorUtils.getColor(R.color.white));
         ForegroundColorSpan redSpan = new ForegroundColorSpan(ColorUtils.getColor(R.color.red_9));
         stringBuilder.setSpan(whiteSpan, 0, whiteLength, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         stringBuilder.setSpan(redSpan, whiteLength, txt.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        stringBuilder.setSpan(new UnderlineSpan(), whiteLength, txt.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         binding.tvInit.setText(stringBuilder);
     }
 }
