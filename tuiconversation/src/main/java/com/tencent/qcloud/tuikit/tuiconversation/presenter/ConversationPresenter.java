789ea55ec4badd974b8f5610845bb469d57d0f35
starpine <subong2013@163.com>
2022-08-04 14:07:36 +0800

    兼容多个管理员账号推送不展示在会话列表

--- tuiconversation/src/main/java/com/tencent/qcloud/tuikit/tuiconversation/presenter/ConversationPresenter.java
+++ tuiconversation/src/main/java/com/tencent/qcloud/tuikit/tuiconversation/presenter/ConversationPresenter.java
@@ -181,7 +181,7 @@ public class ConversationPresenter {
         for (ConversationInfo conversationInfo : conversationInfoList) {
             if (!ConversationUtils.isNeedUpdate(conversationInfo)) {
                 TUIConversationLog.i(TAG, "onNewConversation conversationInfo " + conversationInfo.toString());
-                if (!conversationInfo.getId().equals("customer")){//推送管理员账号不展示在会话列表
+                if (!conversationInfo.getConversationId().contains("customer")){//推送管理员账号不展示在会话列表
                     infos.add(conversationInfo);
                 }
             }
