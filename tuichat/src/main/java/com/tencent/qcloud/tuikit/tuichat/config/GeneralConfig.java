71f9cf7b40653738ebdcac4c1705289ee9d200fc
starpine <subong2013@163.com>
2022-08-05 16:54:09 +0800

    未读标识功能打开

--- tuichat/src/main/java/com/tencent/qcloud/tuikit/tuichat/config/GeneralConfig.java
+++ tuichat/src/main/java/com/tencent/qcloud/tuikit/tuichat/config/GeneralConfig.java
@@ -16,7 +16,7 @@ public class GeneralConfig {
 
     private boolean isAndroidPrivateRing;
 
-    private boolean showRead = false;
+    private boolean showRead = true;
     private boolean reactEnable = false;
     private boolean replyEnable = true;
     private boolean quoteEnable = true;
