056d2c407a4cf588153b218738974da54a62cb94
15616314565 <15616314565@163.com>
2022-08-19 14:26:41 +0800

    推币机功能分支创建

---
+++ app/src/main/java/com/dl/playfun/ui/coinpusher/CoinpusherDialog.java
@@ -0,0 +1,12 @@
+package com.dl.playfun.ui.coinpusher;
+
+import android.app.Dialog;
+
+/**
+ * Author: 彭石林
+ * Time: 2022/8/19 11:16
+ * Description: 推币机弹窗选择页面
+ */
+public class CoinpusherDialog {
+
+}
